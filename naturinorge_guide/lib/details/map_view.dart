85b1011f2de70f6d799a9447a71c3f9e5ae29164
Michal Torma <miso.torma@gmail.com>
2020-01-16 15:57:30 +0100

    Implement clustering on map

--- naturinorge_guide/lib/details/map_view.dart
+++ naturinorge_guide/lib/details/map_view.dart
@@ -6,7 +6,6 @@ import 'package:naturinorge_guide/details/map_provider.dart';
 import 'package:provider/provider.dart';
 
 class MapView extends StatefulWidget {
-  
   MapView({Key key}) : super(key: key);
 
   _MapViewState createState() => _MapViewState();
@@ -19,12 +18,15 @@ class _MapViewState extends State<MapView> {
     return GoogleMap(
       mapType: MapType.terrain,
       markers: Provider.of<MapProvider>(context).markers,
-        initialCameraPosition:
-            CameraPosition(target: LatLng(63.990556, 12.3077779), zoom: 6),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-          Provider.of<MapProvider>(context, listen: false).loadObservationsv2();
-        });
-        
+      initialCameraPosition:
+          CameraPosition(target: LatLng(63.990556, 12.3077779), zoom: 5),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+        Provider.of<MapProvider>(context, listen: false).loadObservations();
+      },
+      onCameraMove: (position) async {
+        Provider.of<MapProvider>(context, listen: false).cameraMoved(position.zoom);
+      },
+    );
   }
 }
