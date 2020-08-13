2a63d03273796cf391a2374ea9518641e34d40cb
Michal Torma <miso.torma@gmail.com>
2020-08-13 14:25:51 +0200

    Add all majort types

---
+++ scripts/database/major_type/f/f3_major_type.py
@@ -0,0 +1,20 @@
+# %%
+from model.model import MajorType, MajorTypeInfo
+
+# %% F3
+f3_major_type = MajorType(
+    _id=3,
+    majorTypeGroup_id='F',
+)
+f3_major_type_en = MajorTypeInfo(
+    language_id='en',
+    majorType=f3_major_type,
+    name='Anoxic lake waterbody',
+    description='Anoxic lake waterbodies comprise the non-circulating, permanently stagnant, oxygen-free waters at the bottom of meromictic lakes (monimolimnion). Meromictic lakes are lakes with a permanent and stable vertical stratification (no seasonal mixing of water between strata under normal conditions). Anoxic waterbodies have characteristic, deviant, chemical composition compared to normal, oxic waters; high concentrations of CO2, CH4, Ca, Fe and/or Mn are typical. Bacteria tend to dominate the species-poor communities.',
+)
+f3_major_type_nb = MajorTypeInfo(
+    language_id='nb',
+    majorType=f3_major_type,
+    name='Ikke-sirkulerende innsjøvannmasser',
+    description='''Ikke-sirkulerende innsjøvannmasser omfatter det permanent stagnerende bunnvannet (monimolimnion) i meromiktiske innsjøer, det vil si innsjøer som har et tungt, kaldt vannlag på bunnen, under sprangsjiktet, oftest på dyp > 10 m, som aldri blander seg med resten av vannet i innsjøen og som derfor er fritt for oksygen. Ikke- sirkulerende vannmasser kjennetegnes av spesielle kjemiske forhold, ofte høye konsentrasjoner av CO2, CH4, Ca, Fe og/eller Mn, en artssammensetning dominert av bakterier og ekstrem artsfattigdom. Innsjøer kan ha permanent stagnerende bunnvann av mange ulike årsaker, og hver enkelt meromiktisk innsjø har derfor unike trekk.'''
+)
---
+++ scripts/database/major_type/f/f4_major_type.py
@@ -0,0 +1,20 @@
+# %%
+from model.model import MajorType, MajorTypeInfo
+
+# %% F4
+f4_major_type = MajorType(
+    _id=4,
+    majorTypeGroup_id='F',
+)
+f4_major_type_en = MajorTypeInfo(
+    language_id='en',
+    majorType=f4_major_type,
+    name='Strongly altered or artificial river waterbody',
+    description='Strongly altered or artificial river waterbodies are characterised by irreversibly altered species composition and ecological function, brought about by human activities such as water regulation, contamination, irreversible eutrophication, chemical treatment against parasites and introduction of exotic species. Thus, the impacts may be physical, chemical or biological.',
+)
+f4_major_type_nb = MajorTypeInfo(
+    language_id='nb',
+    majorType=f4_major_type,
+    name='Sterkt endrete elvevannmasser',
+    description='''Sterkt endrete elvevannmasser omfatter vannmassene i elver som har fått sin artssammensetning og økologiske funksjon sterkt og irreversibelt endret gjennom fysiske inngrep (vassdragsreguleringer), kjemiske inngrep (irreversibel forurensning, eutrofiering, kjemisk behandling mot parasitter etc.) eller biologiske inngrep (utsetting av fiskearter som gjennomgripende har endret den trofiske strukturen og dermed artssammensetningen, etc.).'''
+)
