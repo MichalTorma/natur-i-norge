c5b73ea8eb2289ede32576d5ccdfbea04e5efa85
Michal Torma <miso.torma@gmail.com>
2020-09-04 13:58:55 +0200

    Add elementary segments untill SS

--- scripts/database/lec/ru_lec.py
+++ scripts/database/lec/ru_lec.py
@@ -1,7 +1,6 @@
 # %%
 from model.model import LEC
-from model.model import LECInfo
-from language_data import en_language, nb_language
+from model.model import LECInfo, ElementarySegment, ElementarySegmentInfo
 
 # %% RU
 ru_lec = LEC(
@@ -27,4 +26,212 @@ ru_lec_info_nb = LECInfo(
 	Snøras fører ofte med seg mineralmateriale som er ’friskt’ i den forstand at mineralnæringsstoffer som er lett bundet ikke allerede er vasket ut. Finkornet, friskt forvitringsmateriale av bergarter som på stabil, jorddekket mark gir opphav til et surt og kalkfattig jordsmonn, som for eksempel granitt, har pH omkring 6 og gir opphav til en mineralnæringsrik ras- og skredmarksjord [kalkinnhold (KA) trinn KA∙fg svakt kalkrik].
 	Til grunn for basistrinninndelingen av rasutsatthet (RU) er lagt at dette er en gradient som ender i et artsuttynningsintervall, at artsuttynningsintervallets nedre endetrinn er RU∙c, og at gradientens naturlige øvre endetrinn er der disruptiv forstyrrelse forhindrer en stabil artssammensetning. I henhold til retningslinjene for basistrinndeling av artsuttynningsgradienter (se NiN[2] Artikkel 2, kapittel B2i), skal da rasutsatthet (RU) deles i sju basistrinn.
 ''',
+)
+
+# nul
+nul_elem_segment=ElementarySegment(
+    _id='RU.0',
+    lec=ru_lec,
+    value='0',
+    order=0,
+    relative_order=0
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='without rasp character'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='stable soil that is never affected by landslides and with a species composition that also does not indicate landslide exposure'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='uten raspreg'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='stabil mark som aldri påvirkes av ras og med en artssammensetning som heller ikke indikerer rasutsatthet'
+)
+# a
+a_elem_segment=ElementarySegment(
+    _id='RU.a',
+    lec=ru_lec,
+    value='a',
+    order=1,
+    relative_order=1
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='observably raspy'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='weak but observable (<1 ØAE) effect of landslide impact on species composition (compared to nature not affected by landslide); landslide-exposed forest land, mountain heath and leeward side with an observable sign of landslide impact'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='observerbart raspreget'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='svak, men observerbar (< 1 ØAE) effekt av raspåvirkning på artssammensetningen (sammenliknet med natur som ikke påvirkes av ras); rasutsatt skogsmark, fjellhei og leside med observerbart preg av raspåvirkning'
+)
+# b
+b_elem_segment=ElementarySegment(
+    _id='RU.b',
+    lec=ru_lec,
+    value='b',
+    order=2,
+    relative_order=2
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='a little raspy'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='clear effect of landslide on species composition; in contrast to the adjoining nature which lacks this character'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='litt raspreget'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='klar effekt av ras på artssammensetningen; til forskjell fra tilgrensende natur som mangler dette preget'
+)
+# c
+c_elem_segment=ElementarySegment(
+    _id='RU.c',
+    lec=ru_lec,
+    value='c',
+    order=3,
+    relative_order=3
+)
+ElementarySegmentInfo(
+    elementarySegment=c_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='rather strongly raspy'
+)
+ElementarySegmentInfo(
+    elementarySegment=c_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='very clear effect of landslides on the species composition'
+)
+ElementarySegmentInfo(
+    elementarySegment=c_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='temmelig sterkt raspreget'
+)
+ElementarySegmentInfo(
+    elementarySegment=c_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='meget klar effekt av ras på artssammensetningen'
+)
+# de
+de_elem_segment=ElementarySegment(
+    _id='RU.de',
+    lec=ru_lec,
+    value='de',
+    relative_order=4
+)
+ElementarySegment(
+    _id='RU.d',
+    lec=ru_lec,
+    value='d',
+    order=4,
+    relative_order=0,
+    parent=de_elem_segment
+)
+ElementarySegment(
+    _id='RU.e',
+    lec=ru_lec,
+    value='e',
+    order=5,
+    relative_order=1,
+    parent=de_elem_segment
+)
+ElementarySegmentInfo(
+    elementarySegment=de_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='very strongly raspy'
+)
+ElementarySegmentInfo(
+    elementarySegment=de_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='the effect of landslides is so strong that clear species thinning takes place and rastolerant species dominate'
+)
+ElementarySegmentInfo(
+    elementarySegment=de_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='svært sterkt raspreget'
+)
+ElementarySegmentInfo(
+    elementarySegment=de_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='effekten av ras så sterk at tydelig artsuttynning finner sted og rastolerante arter dominerer'
+)
+# all
+all_elem_segment=ElementarySegment(
+    _id='RU.¤',
+    lec=ru_lec,
+    value='¤',
+    order=6,
+    relative_order=5
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='disturbingly raspy'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='area with such a strong landslide impact that permanent communities of perennial species are not maintained'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='disruptivt raspreg'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='område med så sterk raspåvirkning at permanente samfunn av flerårige arter ikke opprettholdes'
 )
--- scripts/database/lec/se_lec.py
+++ scripts/database/lec/se_lec.py
@@ -1,7 +1,6 @@
 # %%
 from model.model import LEC
-from model.model import LECInfo
-from language_data import en_language, nb_language
+from model.model import LECInfo, ElementarySegment, ElementarySegmentInfo
 
 # %% SE
 se_lec = LEC(
@@ -26,4 +25,133 @@ se_lec_info_nb = LECInfo(
 	Til grunn for basistrinninndelingen av sedimentbasert forstyrrelse (SE) er lagt at dette er en artsuttynningsgradient, der artsuttynningsintervallet starter allerede ved overgangen mellom trinnene ER∙0 og ER∙a og at endepunktet befinner seg der disruptiv forstyrrelse forhindrer en stabil artssammensetning. I henhold til retningslinjene for basistrinndeling av artsuttynningsgradienter (se NiN[2] Artikkel 2, kapittel B2i) skal da sedimentbasert forstyrrelse (SE) deles i fire basistrinn.
 	Også vannforstyrrelsesintensitet (VF) adresserer effekter av vann som forstyrrelsesagens i ferskvannssystemer, men VF adresserer mark/bunn med balanse mellom sedimentasjon og erosjon (SE∙0), og mellom vannets forstyrrelseseffekt og substratets kornstørrelsesfordeling. Sedimentasjonsbasert forstyrrelse (SE) er motsatsen til erosjonsutsatthet (ER) i den forstand at ER adresserer forstyrrelseseffekter av gjentatte erosjonsepisoder.
 ''',
+)
+
+# nul
+nul_elem_segment=ElementarySegment(
+    _id='SE.0',
+    lec=se_lec,
+    value='0',
+    order=0,
+    relative_order=0
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='without sedimentation'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='good mass balance; balance between supply (sedimentation) and removal (erosion) of material'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='uten sedimentasjonspreg'
+)
+ElementarySegmentInfo(
+    elementarySegment=nul_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='god massebalanse; balanse mellom tilførsel (sedimentasjon) og fjerning (erosjon) av materiale'
+)
+# a
+a_elem_segment=ElementarySegment(
+    _id='SE.a',
+    lec=se_lec,
+    value='a',
+    order=1,
+    relative_order=1
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='slightly sedimentary'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='good mass balance; balance between supply (sedimentation) and removal (erosion) of material'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='litt sedimentasjonspreget'
+)
+ElementarySegmentInfo(
+    elementarySegment=a_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='svakt positiv massebalanse, men med artssammensetning som bærer observerbart preg av sedimentasjon'
+)
+# b
+b_elem_segment=ElementarySegment(
+    _id='SE.b',
+    lec=se_lec,
+    value='b',
+    order=2,
+    relative_order=2
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='clearly sedimentation-characterized'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='clearly positive mass balance; the substrate is supplied with clearly more new material by sedimentation than the amount of material eroded away; the species composition consists primarily of sedimentation-tolerant species'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='klart sedimentasjons-preget'
+)
+ElementarySegmentInfo(
+    elementarySegment=b_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='klart positiv massebalanse; substratet tilføres klart mer nytt materiale ved sedimentasjon enn mengden materiale som eroderes vekk; artssammensetningen består først og fremst av sedimentasjonstolerante arter'
+)
+# all
+all_elem_segment=ElementarySegment(
+    _id='SE.¤',
+    lec=se_lec,
+    value='¤',
+    order=3,
+    relative_order=3
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='en',
+    key='<class>',
+    value='characterized by disruptive sedimentation'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='en',
+    key='<description>',
+    value='sedimentation with such a high intensity (combination of strong disturbance and high disturbance frequency) that the establishment and maintenance of permanent populations of site-associated organisms is prevented'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='nb',
+    key='<class>',
+    value='preget av disruptiv sedimentasjon'
+)
+ElementarySegmentInfo(
+    elementarySegment=all_elem_segment,
+    language_id='nb',
+    key='<description>',
+    value='sedimentasjon med så høy intensitet (kombinasjon av sterk forstyrrelsesgrad og høy forstyrrelsesfrekvens) at etablering og opprettholdelse av permanente populasjoner av stedstilknyttete organismer forhindres'
 )
