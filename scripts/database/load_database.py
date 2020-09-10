ff8815502784f5628c6c1492cc9b23e28837bfe1
Michal Torma <miso.torma@gmail.com>
2020-09-10 07:40:43 +0200

    progress on loading new db

--- scripts/database/load_database.py
+++ scripts/database/load_database.py
@@ -20,7 +20,7 @@ engine = create_engine(f'sqlite:///{database_file}')
 create_query = Base.metadata.create_all(engine)
 session = sessionmaker(bind=engine)()
 
-#%% chreate schema
+#%% create schema
 import subprocess
 import sadisplay
 import codecs
@@ -40,6 +40,7 @@ subprocess.getoutput('dot -Tpng schema.dot > schema.png')
 import pandas as pd
 path = 'tables/MasterTable.xlsx'
 
+language = pd.read_excel(path, sheet_name='Language')
 majorTypeGroup = pd.read_excel(path, sheet_name='MajorTypeGroup')
 majorType = pd.read_excel(path, sheet_name='MajorType')
 minorType = pd.read_excel(path, sheet_name='MinorType')
@@ -52,6 +53,15 @@ elementarySegment = pd.read_excel(path, sheet_name='ElementarySegment')
 minorTypeStandardSegment = pd.read_excel(path, sheet_name='MinorTypeStandardSegment')
 minorTypeScale = pd.read_excel(path, sheet_name='MinorTypeScale')
 standardSegment = pd.read_excel(path, sheet_name='StandardSegment')
+# %% Language
+for lang_tuple in language.iterrows():
+    lang = lang_tuple[1]
+    session.add(model.Language(
+        _id=lang._id,
+        name=lang.name
+    ))
+    session.commit()
+
 # %% MajorTypeGroup
 for mtg_tuple in majorTypeGroup.iterrows():
     mtg = mtg_tuple[1]
@@ -84,6 +94,134 @@ for mtg_tuple in majorTypeGroup.iterrows():
         value=mtg['<description>|nb']
     ))
     session.commit()
-# %%
-majorTypeGroup
+# %% MajorType
+for mt_tuple in majorType.iterrows():
+    mt = mt_tuple[1]
+    desc_id = f'major_type_{mt._id}'
+    session.add(
+        model.MajorType(
+            _id= mt._id,
+            majorTypeGroup_id = mt.majorTypeGroup_id,
+            order = mt.num,
+            detail_id = desc_id
+
+        )
+    )
+    session.add(
+        model.Detail(
+            _id = desc_id,
+            key = '<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+    session.add(
+        model.Detail(
+            _id = desc_id,
+            key = '<name>',
+            language_id = 'nb',
+            value = mt['<name>|nb']
+        )
+    )
+    session.add(
+        model.Detail(
+            _id = desc_id,
+            key = '<description>',
+            language_id = 'en',
+            value = mt['<description>|en']
+        )
+    )
+    session.add(
+        model.Detail(
+            _id = desc_id,
+            key = '<description>',
+            language_id = 'nb',
+            value = mt['<description>|nb']
+        )
+    )
+    session.commit()
+# %% MinorType
+for mt_tuple in minorType.iterrows():
+    mt = mt_tuple[1]
+    detail_id = f'minor_type_{mt._id}'
+    session.add(
+        model.MinorType(
+            _id = mt._id,
+            order = mt.order,
+            majorType_id = mt.majorType_id,
+            detail_id = detail_id
+        )
+    )
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'nb',
+            value = mt['<name>|nb']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='Ecological characteristics',
+            language_id = 'en',
+            value = mt['Ecological characteristics|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'nb',
+            value = mt['<name>|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+
+    session.add(
+        model.Detail(
+            _id = detail_id,
+            key='<name>',
+            language_id = 'en',
+            value = mt['<name>|en']
+        )
+    )
+
 # %%
