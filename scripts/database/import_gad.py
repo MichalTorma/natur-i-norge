caf98f6c4cd96e22acc559986b760c929e3086e4
Michal Torma <miso.torma@gmail.com>
2020-10-20 14:22:30 +0200

    Refactor DB

--- scripts/database/import_gad.py
+++ scripts/database/import_gad.py
@@ -5,10 +5,37 @@ import model.model as model
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_path = 'db/v005.db'
+db_path = 'db/v006.db'
 major_type = 'T-4'
-# %%
 
+# Get local data
+local_data = pd.read_excel('tables/GAD3v2.xlsm', sheet_name='GADml3', header=1)
+local_data['Art'] = local_data['Art'].apply(
+    lambda x: x.replace(' ssp.', '').replace(' spp.','').strip())
+
+# Establish session
+engine = create_engine(f'sqlite:///{db_path}')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Global function for appropriate local row equivalent
+def get_local(specie):
+    latin_name = specie.scientificName
+    local_specie = local_data[local_data['Art'] == latin_name]
+    norwegian_name = specie.vernacularName
+    if len(local_specie) != 1:
+        local_specie = local_data[local_data['NorskNavn'] == norwegian_name]
+        if len(local_specie) != 1:
+            raise Exception(f' Unable to find: {latin_name}')
+        # print(f'{latin_name} - {local_specie.Art}')
+    return local_specie
+# %% Delete All
+# session.query(model.GadValue).delete()
+# session.query(model.Species).delete()
+# session.query(model.ElementarySegmentGroupCombination).delete()
+session.query(model.GadModifier).delete()
+session.commit()
+# %% Get Species from API
 url = "https://traitsapi.artsdatabanken.no/api/Species"
 
 payload = {}
@@ -17,31 +44,15 @@ headers = {}
 species_response = requests.request("GET", url, headers=headers, data=payload)
 
 species = species_response.json()
-# %%
-local_data = pd.read_excel('tables/GAD3v2.xlsm', sheet_name='GADml3', header=1)
-local_data['Art'] = local_data['Art'].apply(
-    lambda x: x.replace(' ssp.', '').replace(' spp.','').strip())
-# %%
 
+# %% Get traits per specie from API
 def get_traits_api(specie):
     url = f'https://traitsapi.artsdatabanken.no/byScientificNameId/{specie["scientificNameId"]}'
 
     response = requests.request("GET", url, headers=headers, data=payload)
     return response.json()
 
-# get_traits_api(species[0])
 
-
-engine = create_engine(f'sqlite:///{db_path}')
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# %%
-session.query(model.GadValue).delete()
-session.query(model.Species).delete()
-session.query(model.ElementarySegmentGroupCombination).delete()
-session.commit()
-# %%
 def insert_specie_gad(specie):
     traits_api = get_traits_api(specie)['speciesNaturetypeResource']
     session.add(
@@ -77,27 +88,13 @@ def insert_specie_gad(specie):
     session.commit()
 
 
-# %%
 for specie in species:
     insert_specie_gad(specie)
-# %%
+# %% Get local excel file to supplement API data
 
 
-def get_local(specie):
-    latin_name = specie.scientificName
-    local_specie = local_data[local_data['Art'] == latin_name]
-    norwegian_name = specie.vernacularName
-    if len(local_specie) != 1:
-        local_specie = local_data[local_data['NorskNavn'] == norwegian_name]
-        if len(local_specie) != 1:
-            raise Exception(f' Unable to find: {latin_name}')
-        # print(f'{latin_name} - {local_specie.Art}')
-    return local_specie
 
-# %%
-
-
-def insert_gad_value(specie, elementary_segments, value):
+def insert_gad_value(specie, elementary_segments, m7_value=None):
     combination_id = "/".join(elementary_segments)
     for es in elementary_segments:
         session.merge(
@@ -106,83 +103,81 @@ def insert_gad_value(specie, elementary_segments, value):
                 elementarySegmentGroup_id=es
             )
         )
+    
     session.add(
         model.GadValue(
             elementarySegmentGroupCombination_id=combination_id,
             species_id=specie.scientificNameId,
             majorType_id=major_type,
-            valueM7Scale_id=int(value)
+            valueM7Scale_id=m7_value,
         )
     )
-
-# %%
-
 
 def insert_ki(specie, local):
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.d', 'UF.a', 'KI.bc'],
-        value=local['K3'].values[0]
+        m7_value=local['K3'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.d', 'UF.b', 'KI.bc'],
-        value=local['K3'].values[0]
+        m7_value=local['K3'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.e', 'UF.a', 'KI.bc'],
-        value=local['K4'].values[0]
+        m7_value=local['K4'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.e', 'UF.b', 'KI.bc'],
-        value=local['K4'].values[0]
+        m7_value=local['K4'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.f', 'UF.a', 'KI.bc'],
-        value=local['K5'].values[0]
+        m7_value=local['K5'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.f', 'UF.b', 'KI.bc'],
-        value=local['K5'].values[0]
+        m7_value=local['K5'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.g', 'UF.a', 'KI.bc'],
-        value=local['K6'].values[0]
+        m7_value=local['K6'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.g', 'UF.b', 'KI.bc'],
-        value=local['K6'].values[0]
+        m7_value=local['K6'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.h', 'UF.a', 'KI.bc'],
-        value=local['K7'].values[0]
+        m7_value=local['K7'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.h', 'UF.b', 'KI.bc'],
-        value=local['K7'].values[0]
+        m7_value=local['K7'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.i', 'UF.a', 'KI.bc'],
-        value=local['K8'].values[0]
+        m7_value=local['K8'].values[0]
     )
     insert_gad_value(
         specie=specie,
         elementary_segments=['KA.i', 'UF.b', 'KI.bc'],
-        value=local['K8'].values[0]
+        m7_value=local['K8'].values[0]
     )
     # print(local['K8'].values[0])
     session.commit()
 
-# %%
+# Add supplemental value from local
 
 def add_local_supplement(specie):
     local = get_local(specie)
@@ -199,4 +194,29 @@ for specie in all_species:
         add_local_supplement(specie)
     except Exception as e:
         print(f'{specie.scientificName}\n{e}')
+# %% Insert supplementary LEC
+def insert_modifier(value, lec_id, specie_id):
+    if value != None or value != '-x':
+        session.add(
+            model.GadModifier(
+                lec_id=lec_id,
+                specie_id=specie_id,
+                value=value
+            )
+        )
+
+def insert_modifiers(specie, local):
+    insert_modifier(
+        value=local['VM'],
+        lec_id='VM',
+        specie_id=specie.scientificNameId
+    )    
+
+def add_local_modifier(specie):
+    local = get_local(specie)
+    insert_modifiers(specie,local)
+    return local
+
+local = add_local_modifier(session.query(model.Species).first())
+
 # %%
