064146ecdd588b294c67a313ca32c1e410a76f39
Michal Torma <miso.torma@gmail.com>
2020-12-01 14:37:58 +0100

    Change accent color

--- naturinorge_guide/lib/tools/my_theme.dart
+++ naturinorge_guide/lib/tools/my_theme.dart
@@ -22,13 +22,14 @@ ThemeData myTheme = ThemeData(
 );
 
 NeumorphicThemeData lightTheme = NeumorphicThemeData(
-  baseColor: Color(0xFFFFFFFF),
-  lightSource: LightSource.topLeft,
-  depth: 10,
-);
+    baseColor: Color(0xFFFFFFFF),
+    lightSource: LightSource.topLeft,
+    depth: 10,
+    accentColor: Colors.green);
 
 NeumorphicThemeData darkTheme = NeumorphicThemeData(
   baseColor: Color(0xFF3E3E3E),
   lightSource: LightSource.topLeft,
+  textTheme: TextTheme(),
   depth: 6,
 );
