ae6e8c3b4198f5c555d200b2e21c5b4feee070db
Michal Torma <miso.torma@gmail.com>
2021-01-19 12:50:15 +0100

    Fix typo

--- naturinorge_guide/lib/pages/nin_structure/major_type/minor_type_table/table_body.dart
+++ naturinorge_guide/lib/pages/nin_structure/major_type/minor_type_table/table_body.dart
@@ -15,26 +15,32 @@ class TableBody extends StatelessWidget {
       );
     }
     var minorTypesBlocks = Provider.of<MajorTypeProvider>(context)
-        .minorTypeSclaedBlocks
+        .minorTypeScaledBlocks
         .map((e) => ScaledMinorTypeWidgetWidget(minorTypeBlock: e))
         .toList();
     var staggeredTiles = Provider.of<MajorTypeProvider>(context)
-        .minorTypeSclaedBlocks
+        .minorTypeScaledBlocks
         .map((e) => StaggeredTile.count(e.width, e.height))
         .toList();
-    return StaggeredGridView.count(
-      addAutomaticKeepAlives: true,
-      padding: EdgeInsets.all(4.0),
-      crossAxisSpacing: 2.0,
-      mainAxisSpacing: 2.0,
-      physics: new NeverScrollableScrollPhysics(),
-      crossAxisCount:
-          Provider.of<MajorTypeProvider>(context).xAxis.standardSegments.length,
-      reverse: true,
-      primary: true,
-      // shrinkWrap: true,
-      children: minorTypesBlocks,
-      staggeredTiles: staggeredTiles,
+    return Stack(
+      children: [
+        StaggeredGridView.count(
+          addAutomaticKeepAlives: true,
+          padding: EdgeInsets.all(4.0),
+          crossAxisSpacing: 2.0,
+          mainAxisSpacing: 2.0,
+          physics: new NeverScrollableScrollPhysics(),
+          crossAxisCount: Provider.of<MajorTypeProvider>(context)
+              .xAxis
+              .standardSegments
+              .length,
+          reverse: true,
+          primary: true,
+          // shrinkWrap: true,
+          children: minorTypesBlocks,
+          staggeredTiles: staggeredTiles,
+        ),
+      ],
     );
   }
 }
