be3a19698114e604fa73175123267ca0bd68b18a
Michal Torma <miso.torma@gmail.com>
2021-02-03 03:07:18 +0100

    Show minor type details

--- naturinorge_guide/lib/pages/nin_structure/major_type/minor_type_table/scaled_minor_type.dart
+++ naturinorge_guide/lib/pages/nin_structure/major_type/minor_type_table/scaled_minor_type.dart
@@ -1,42 +1,49 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table/minor_type_block.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table/minor_type_description.dart';
 
-class ScaledMinorTypeWidgetWidget extends StatelessWidget {
+class ScaledMinorTypeWidget extends StatelessWidget {
   final MinorTypeBlock minorTypeBlock;
-  const ScaledMinorTypeWidgetWidget({Key key, this.minorTypeBlock})
-      : super(key: key);
+  const ScaledMinorTypeWidget({Key key, this.minorTypeBlock}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (minorTypeBlock.minorTypeScaled == null) {
       return Container();
     } else
-      return Neumorphic(
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Center(
-                child: AutoSizeText(
-                    minorTypeBlock.minorTypeScaled.minorTypeScaledId),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Center(
-                child: AutoSizeText(
-                  minorTypeBlock.minorTypeScaled.minorTypes
-                      .map((e) => e.minorType.name)
-                      .toList()
-                      .join('/'),
-                  textAlign: TextAlign.center,
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 3,
+      return GestureDetector(
+        onTap: () => showDialog(
+            context: context,
+            builder: (context) => MinorTypeDescription(
+                  minorTypes: minorTypeBlock.minorTypeScaled.minorTypes,
+                )),
+        child: Neumorphic(
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Center(
+                  child: AutoSizeText(
+                      minorTypeBlock.minorTypeScaled.minorTypeScaledId),
                 ),
               ),
-            )
-          ],
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: AutoSizeText(
+                    minorTypeBlock.minorTypeScaled.minorTypes
+                        .map((e) => e.minorType.name)
+                        .toList()
+                        .join('/'),
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.ellipsis,
+                    maxLines: 3,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       );
   }
