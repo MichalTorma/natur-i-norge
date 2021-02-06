992c5b884aa7e3d217a110c4797f512de83d17ac
Michal Torma <miso.torma@gmail.com>
2021-02-06 15:46:23 +0100

    Setup model for NiN

--- scripts/nin_struktur/nin/admin.py
+++ scripts/nin_struktur/nin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from nin.models import NinLanguage
+
 
 # Register your models here.
+@admin.register(NinLanguage)
+class NinLanguageAdmin(admin.ModelAdmin):
+    pass
