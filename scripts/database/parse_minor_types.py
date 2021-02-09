7638fa9afd65216052e8038f1d5939675940f40e
Michal Torma <miso.torma@gmail.com>
2021-02-09 11:10:51 +0100

    Add dLEC to db

--- scripts/database/parse_minor_types.py
+++ scripts/database/parse_minor_types.py
@@ -12,6 +12,7 @@ segments_table = pd.read_excel('tables/MinorTypes.xlsx', sheet_name="MajorTypeSe
 minor_types_table = pd.read_excel('tables/MinorTypes.xlsx', sheet_name="MinorTypes")
 major_types = list(set(list(segments_table.major_type)))
 
+major_type = major_types[0]
 table = segments_table[segments_table.major_type == major_type]
 table.standard_segments = table.standard_segments.apply(lambda x: x.replace(' ','')
         .replace('(','')
@@ -19,13 +20,7 @@ table.standard_segments = table.standard_segments.apply(lambda x: x.replace(' ',
         .replace('∙','.'))
 lec_type = 3
 lkm_strings = table.standard_segments[table.lec_type == lec_type]
-for major_type in major_types:
-    print(major_type)
-    # try:
-    get_dLKM(major_type)
-    # except Exception as e:
-    #     print('Error')
-    #     print(e)
+
 # %%
 lkm_list = ';'.join(list(lkm_strings)).replace(';;',';').split(';')
 assert len(lkm_list) > 0
@@ -53,7 +48,7 @@ def get_dLKM(major_type):
     for dLKM_item in dLKM_list:
         dLKM_gradient = dLKM_item.split('.')[0]
         dLKM_segments_string = dLKM_item.split('.')[1]
-        print(dLKM_segments_string)
+        # print(dLKM_segments_string)
         es_q = session.query(model.ElementarySegment).filter(model.ElementarySegment.lec_id == dLKM_gradient)
         if es_q.count() == 0:
             raise Exception(f'Not yet imported elementary segments for {dLKM_gradient}')
@@ -66,11 +61,44 @@ def get_dLKM(major_type):
         if(len(dLKM_segments_string) > 2):
             raise Exception(f'wrong number of leters in dLKM definition major_type: {major_type} string:{dLKM_segments_string}')
 
-        dLKM_es =es_q.filter(model.ElementarySegment.order >= es_start.order).filter(model.ElementarySegment.order <= es_end.order).all()
+        dLKM_es_q =es_q.filter(model.ElementarySegment.order >= es_start.order).filter(model.ElementarySegment.order <= es_end.order)
+        dLKM_es = dLKM_es_q.order_by(model.ElementarySegment.order).all()
         assert len(dLKM_es) > 0
+        # add majorTypeLec
+        major_type_nh = major_type.replace('-','')
+        mtl_id = f'{major_type_nh}-{dLKM_gradient}'
+        session.merge(model.MajorTypeLEC(
+            _id=mtl_id,
+            lec_id=dLKM_gradient,
+            majorType_id=major_type,
+            lecType_id='dLEC',
+        ))
+        session.commit()
+        es_id_list = []
+        for es in dLKM_es:
+            es_id_list.append(es.value)
+        es_ids_string = ''.join(es_id_list)
+        ss_id = f'{major_type_nh}-{dLKM_gradient}.{es_ids_string}'
+        session.merge(model.StandardSegment(
+            _id=ss_id,
+            majorTypeLEC_id=mtl_id,
+            ))
+        session.commit()
+        for es in dLKM_es:
+            session.merge(model.StandardSegmentElement(
+                standardSegment_id=ss_id,
+                elementarySegment_id=es._id
+            ))
+            session.commit()
 # save elementary segments to MajorTypeLec
 # %%
-
+for major_type in major_types:
+    print(major_type)
+    # try:
+    get_dLKM(major_type)
+    # except Exception as e:
+    #     print('Error')
+    #     print(e)
 # %%
 get_dLKM('L-1')
 # %%
