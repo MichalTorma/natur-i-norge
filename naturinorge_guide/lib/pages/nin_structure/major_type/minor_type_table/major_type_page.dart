2fa239cc19bf9dd5bac0fb9c239f85d33efde476
Michal Torma <miso.torma@gmail.com>
2021-02-24 14:14:39 +0100

    Switch to pages

---
+++ naturinorge_guide/lib/pages/nin_structure/major_type/minor_type_table/major_type_page.dart
@@ -0,0 +1,45 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:naturinorge_guide/pages/nin_structure/app_bar/nin_app_bar.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/major_type_description.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/major_type_description_landscape.dart';
+import 'package:naturinorge_guide/pages/nin_structure/nin_structure_provider.dart';
+import 'package:provider/provider.dart';
+
+class MajorTypePage extends StatelessWidget {
+  const MajorTypePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var mt = Provider.of<NinStructureProvider>(
+      context,
+    ).selectedMajorType;
+    return Scaffold(
+      appBar: NeumorphicAppBar(
+        title: AutoSizeText(
+          '${mt.data.id} ${mt.name}',
+          style: Theme.of(context).textTheme.headline3,
+        ),
+      ),
+      // appBar: PreferredSize(
+      //     preferredSize: Size(MediaQuery.of(context).size.width, 60),
+      //     child: NinAppBar()),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: LayoutBuilder(
+            // stream: null,
+            builder: (context, boxConstraints) {
+          if (boxConstraints.maxWidth < 800) {
+            return MajorTypeDetails(
+              ninMajorType: mt,
+            );
+          } else {
+            return MajorTypeDetailsLandscape(
+              ninMajorType: mt,
+            );
+          }
+        }),
+      ),
+    );
+  }
+}
---
+++ naturinorge_guide/lib/pages/nin_structure/major_type_group_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
+import 'package:naturinorge_guide/pages/nin_structure/app_bar/nin_app_bar.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type_group/major_type_group_selector.dart';
+
+class MajorTypeGroupPage extends StatelessWidget {
+  const MajorTypeGroupPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          NinAppBar(),
+          SliverToBoxAdapter(
+            child: MajorTypeGroupSelector(),
+          ),
+        ],
+      ),
+    );
+  }
+}
