2d4cc4606d36a9eff1ced6f2943f69580d3c874c
Michal Torma <miso.torma@gmail.com>
2021-02-28 22:31:00 +0100

    Add species list

---
+++ naturinorge_guide/lib/pages/species/species_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:naturinorge_guide/db/nin_db.dart';
+import 'package:naturinorge_guide/main.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/major_type_provider.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table/major_type_page.dart';
+import 'package:provider/provider.dart';
+import 'package:easy_localization/easy_localization.dart';
+
+class SpeciesPage extends StatefulWidget {
+  const SpeciesPage({Key key}) : super(key: key);
+
+  @override
+  _SpeciesPageState createState() => _SpeciesPageState();
+}
+
+class _SpeciesPageState extends State<SpeciesPage> {
+  List<NinSpecie> species = List<NinSpecie>.empty(growable: true);
+
+  @override
+  void initState() {
+    db.getSpeciesByFilter('').then((value) => setState(() {
+          species = value;
+        }));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: TextFormField(
+              decoration: InputDecoration(
+                icon: Icon(Icons.search),
+                hintText: 'Søk',
+              ),
+              // initialValue: ,
+              autocorrect: false,
+              onChanged: (filter) async {
+                var newSpecies = await db.getSpeciesByFilter(filter);
+                setState(() {
+                  species = newSpecies;
+                });
+              }),
+        ),
+      ),
+      body: ListView.builder(
+          itemCount: species.length,
+          itemBuilder: (context, index) => ListTile(
+                leading: Text(species[index].scientificName),
+                subtitle: Text(species[index].vernacularName),
+                onTap: () async {
+                  var specie = species[index];
+                  var mts =
+                      await db.getMajorTypesForSpecie(specie, context.locale);
+                  await Provider.of<MajorTypeProvider>(context, listen: false)
+                      .load(mts[0]);
+                  await Provider.of<MajorTypeProvider>(context, listen: false)
+                      .addSpecies(species[index]);
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => MajorTypePage(
+                                majorType: mts[0],
+                              )));
+                },
+              )),
+    );
+  }
+}
