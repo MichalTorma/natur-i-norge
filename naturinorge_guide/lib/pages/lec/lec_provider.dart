4bd75093afa83edb34c0710ac33c33f1c39c68f7
Michal Torma <miso.torma@gmail.com>
2021-02-28 21:48:24 +0100

    Add lec page

---
+++ naturinorge_guide/lib/pages/lec/lec_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:naturinorge_guide/db/db_adapters.dart';
+import 'package:naturinorge_guide/main.dart';
+
+class LecProvider extends ChangeNotifier {
+  var _lecAdapters = List<LecAdapter>.empty(growable: true);
+  final Locale locale;
+
+  LecProvider(this.locale);
+
+  Future initalize() async {
+    if (_lecAdapters.length > 0) {
+      return;
+    }
+    var allLec = await db.getAllLec();
+    var lecAdapters = allLec.map((e) => LecAdapter(locale, e));
+    for (var lec in lecAdapters) {
+      await lec.getRelations();
+      _lecAdapters.add(lec);
+    }
+    notifyListeners();
+  }
+
+  List<LecAdapter> get lecAdapters => _lecAdapters;
+}
---
+++ naturinorge_guide/lib/pages/nin_structure/other/loading_overlay.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/major_type_provider.dart';
+import 'package:naturinorge_guide/pages/nin_structure/nin_structure_provider.dart';
+import 'package:provider/provider.dart';
+
+class LoadingOverlay extends StatelessWidget {
+  const LoadingOverlay({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isLoading = Provider.of<NinStructureProvider>(context).isLoading ||
+        Provider.of<MajorTypeProvider>(context).isLoading;
+    if (isLoading) {
+      var size = MediaQuery.of(context).size;
+      return AbsorbPointer(
+        child: Container(
+          height: size.height - kToolbarHeight,
+          width: size.width,
+          color: Colors.black.withOpacity(0.8),
+        ),
+      );
+    } else {
+      return Container();
+    }
+  }
+}
