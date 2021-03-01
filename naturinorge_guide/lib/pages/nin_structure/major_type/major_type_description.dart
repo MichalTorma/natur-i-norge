27649f0def8781a50c23b6d7a09b0c98aa0b6822
Michal Torma <miso.torma@gmail.com>
2021-03-01 20:40:50 +0100

    Add reset for standard types

--- naturinorge_guide/lib/pages/nin_structure/major_type/major_type_description.dart
+++ naturinorge_guide/lib/pages/nin_structure/major_type/major_type_description.dart
@@ -8,6 +8,7 @@ import 'package:naturinorge_guide/pages/nin_structure/major_type/major_type_prov
 import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table.dart';
 import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table/secondary_axis_dialog.dart';
 import 'package:naturinorge_guide/pages/nin_structure/major_type/minor_type_table/table_options.dart';
+import 'package:naturinorge_guide/pages/nin_structure/major_type/standard_segments_reset.dart';
 import 'package:provider/provider.dart';
 
 class MajorTypeDetails extends StatelessWidget {
