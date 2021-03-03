02fbc8da93194a20a652a5a41ccc79fd261d767e
Michal Torma <miso.torma@gmail.com>
2021-03-03 13:57:44 +0100

    Hotfix T14

--- naturinorge_guide/lib/db/nin_db.dart
+++ naturinorge_guide/lib/db/nin_db.dart
@@ -319,7 +319,7 @@ class NiNDatabase extends _$NiNDatabase {
   }
 
   @override
-  int get schemaVersion => 52;
+  int get schemaVersion => 53;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(onCreate: (Migrator m) {
