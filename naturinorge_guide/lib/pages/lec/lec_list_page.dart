5c35e577b95a4c0b182cf0d59dd8d5563c1a66ad
Michal Torma <miso.torma@gmail.com>
2021-03-05 13:32:08 +0100

    Clean DB and bump up version

--- naturinorge_guide/lib/pages/lec/lec_list_page.dart
+++ naturinorge_guide/lib/pages/lec/lec_list_page.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:naturinorge_guide/db/db_adapters.dart';
 import 'package:naturinorge_guide/pages/lec/lec_detail_page.dart';
 import 'package:naturinorge_guide/pages/lec/lec_provider.dart';
+import 'package:naturinorge_guide/pages/nin_structure/other/loading.dart';
+import 'package:naturinorge_guide/tools/global_vars.dart';
 import 'package:provider/provider.dart';
 
 class LecListPage extends StatelessWidget {
@@ -12,14 +14,23 @@ class LecListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Provider.of<LecProvider>(context, listen: false).initalize();
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('LEC'),
+          bottom: LoadingWidget(),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () => Navigator.of(context)
+                    .popUntil((route) => !route.navigator.canPop()))
+          ],
+        ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: GridView.extent(
             primary: false,
             shrinkWrap: true,
-            maxCrossAxisExtent: 200,
-            childAspectRatio: 2,
+            maxCrossAxisExtent: GRIDVIEW_CROSS_AXIS_EXTENT,
+            childAspectRatio: GRIDVIEW_ASPECT_RATIO,
             clipBehavior: Clip.none,
             children: Provider.of<LecProvider>(context)
                 .lecAdapters
