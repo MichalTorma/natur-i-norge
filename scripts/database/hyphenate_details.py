9bd694dcc1b89b725ba9f2cce7f1e53c99bfe674
Michal Torma <miso.torma@gmail.com>
2021-05-27 11:09:53 +0200

    Pre hyphenation commit

---
+++ scripts/database/hyphenate_details.py
@@ -0,0 +1,42 @@
+# %%
+import pandas as pd
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model.model import Base
+import model.model as model
+import pyphen
+
+# %%
+pyphen.language_fallback('nn_NO')
+dic = pyphen.Pyphen(lang='nb_NO')
+# %%
+res = dic.inserted('ikke saltanriket', hyphen='-')# hyphen='\u00AD')
+print(res)
+# %%
+database_file = '/Users/amarok/src/natur-i-norge/naturinorge_guide/assets/nin_database.db'
+engine = create_engine(f'sqlite:///{database_file}')
+# %%
+Session = sessionmaker(bind=engine)
+session = Session()
+# %%
+details = session.query(model.Detail)\
+    .filter(model.Detail.language_id == 'nb').all()
+# %%
+def hyphenate_word(word):
+    if word == None:
+        return None
+    if '\u00AD' in word:
+        return word
+
+    return dic.inserted(word, hyphen='\u00AD')
+
+def hyphenate_detail(detail):
+    if detail.key[0] != '<':
+        detail.key = hyphenate_word(detail.key)
+    detail.value = hyphenate_word(detail.value)
+    session.commit()
+
+# %%
+for detail in details:
+    hyphenate_detail(detail)
+# %%
