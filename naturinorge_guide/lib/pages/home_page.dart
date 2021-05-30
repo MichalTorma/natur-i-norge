d0e2604ff577df1ec184d02b793fcb69c2d4917b
Michal Torma <miso.torma@gmail.com>
2021-05-30 17:58:53 +0200

    Change about us section

--- naturinorge_guide/lib/pages/home_page.dart
+++ naturinorge_guide/lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:naturinorge_guide/generated/codegen_loader.g.dart';
@@ -9,6 +10,7 @@ import 'package:naturinorge_guide/pages/nin_structure/other/loading.dart';
 import 'package:naturinorge_guide/pages/nin_structure/other/loading_overlay.dart';
 import 'package:naturinorge_guide/pages/species/species_page.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // const _mail_url = 'mailto:miso.torma.dev@gmail.com';
 
@@ -27,7 +29,7 @@ class HomePage extends StatelessWidget {
           actions: [
             PopupMenuButton<int>(
               itemBuilder: (context) => [
-                PopupMenuItem<int>(value: 0, child: Text('Om os')),
+                PopupMenuItem<int>(value: 0, child: Text('Om NiN-Guide')),
               ],
               onSelected: (idx) => showAboutDialog(
                   context: context,
@@ -42,6 +44,40 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   children: [
+                    Text(
+                        'Natur i Norge Guide er utviklet av Naturhistorisk Museum ved Universitetet i Oslo (NHM) i samarbeid med Artsdatabanken. Appen er et hjelpemiddel for naturkartleggere i arbeidet med å typifisere norsk natur etter typesystemet Natur i Norge.'),
+                    Text(
+                        'Appen ligger nå ute i en testversjon, og inneholder foreløpig kun art-/habitatrelasjoner for utvalgte arter i skogsmark. Datagrunnlaget appen bygger på er utarbeidet av NHM med bistand fra flere norske fagmiljø under utviklingen av Natur i Norge versjon 2. Dette datagrunnlaget er også tilgjengelig via'),
+                    ElevatedButton(
+                      onPressed: () async => await launch(
+                          'https://artsdatabanken.no/egenskapsbanken'),
+                      // onPressed: () async => await canLaunch(_mail_url)
+                      //     ? await launch(_mail_url)
+                      //     : throw 'Could not launch $_mail_url',
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text('artsdatabanken.no'),
+                          Icon(Icons.launch)
+                        ],
+                      ),
+                    ),
+                    Text(
+                        'Natur i Norge Guide er tilgjengelig for nedlasting som Android- og iOS-app:'),
+                    ElevatedButton(
+                      onPressed: () async =>
+                          await launch('https://l.ead.me/batwGC'),
+                      // onPressed: () async => await canLaunch(_mail_url)
+                      //     ? await launch(_mail_url)
+                      //     : throw 'Could not launch $_mail_url',
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [Text('App link'), Icon(Icons.launch)],
+                      ),
+                    ),
+
                     Text('Prof. Rune Halvorsen'),
                     Text('Michal Torma'),
                     Text('Artsdatabanken'),
