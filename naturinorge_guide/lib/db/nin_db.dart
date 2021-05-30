470526878406f784ad498d788209226a3b0f260b
Michal Torma <miso.torma@gmail.com>
2021-05-30 17:58:53 +0200

    Change about us section

--- naturinorge_guide/lib/db/nin_db.dart
+++ naturinorge_guide/lib/db/nin_db.dart
@@ -323,7 +323,7 @@ class NiNDatabase extends _$NiNDatabase {
   }
 
   @override
-  int get schemaVersion => 76;
+  int get schemaVersion => 77;
 
   @override
   MigrationStrategy get migration => MigrationStrategy(onCreate: (Migrator m) {
