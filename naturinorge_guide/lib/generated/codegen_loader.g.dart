660c72edfd04749659606ff0235a7bfb018dd24c
Michal Torma <miso.torma@gmail.com>
2021-09-08 10:07:50 +0200

    Update libraries

--- naturinorge_guide/lib/generated/codegen_loader.g.dart
+++ naturinorge_guide/lib/generated/codegen_loader.g.dart
@@ -1,16 +1,16 @@
 // DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
 
-abstract class LocaleKeys {
-  static const app_name = 'app_name';
+abstract class  LocaleKeys {
+  static const application_name = 'application_name';
   static const toggle_theme = 'toggle_theme';
   static const structure = 'structure';
   static const T4_helper = 'T4_helper';
   static const read_mode = 'read_mode';
   static const show_less = 'show_less';
-  // static const __name__ = '__name__';
-  // static const __description__ = '__description__';
-  // static const __ecological_characteristics__ = '__ecological_characteristics__';
-  // static const __physiognomy__ = '__physiognomy__';
-  // static const __terrain_and_aerial_photo_characteristics__ = '__terrain_and_aerial_photo_characteristics__';
+  static const __name__ = '__name__';
+  static const __description__ = '__description__';
+  static const __ecological_characteristics__ = '__ecological_characteristics__';
+  static const __physiognomy__ = '__physiognomy__';
+  static const __terrain_and_aerial_photo_characteristics__ = '__terrain_and_aerial_photo_characteristics__';
 
 }
