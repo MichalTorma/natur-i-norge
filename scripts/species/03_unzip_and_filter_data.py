a8ea33ea673bbbcc9887061522dbff49c191486d
Michal Torma <miso.torma@gmail.com>
2021-09-16 13:50:59 +0200

    Extract only media and clean before extraction

--- scripts/species/03_unzip_and_filter_data.py
+++ scripts/species/03_unzip_and_filter_data.py
@@ -1,6 +1,7 @@
 # %%
 from glob import glob
 import os
+import shutil
 from zipfile import ZipFile
 import logging
 # %%
@@ -9,6 +10,15 @@ import logging
 # %%
 dirs_in_data = glob('data/*')
 # %%
+def clear_dir(dir):
+    all_files_in_dir = set(glob(f'{dir}{os.sep}*'))
+    zip_files_in_dir = set(glob(f'{dir}{os.sep}*.zip'))
+
+    files_to_remove = list(all_files_in_dir.difference(zip_files_in_dir))
+    # print(files_to_remove)
+    for file_to_remove in files_to_remove:
+        os.remove(file_to_remove)
+
 def unzip(dir):
     files_in_dir = glob(f'{dir}/*.zip')
     if len(files_in_dir) == 0:
@@ -19,11 +29,13 @@ def unzip(dir):
         return
 
     with ZipFile(files_in_dir[0], 'r') as zip_object:
-        zip_object.extractall(dir)
+        zip_object.extract('multimedia.txt', f'{dir}')
 
 # unzip(dirs_in_data[1])
 # %%
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     for data_dir in dirs_in_data:
+        clear_dir(data_dir)
         unzip(data_dir)
+        print('.', end='')
