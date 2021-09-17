5695a9a0a0cc1e715ee8582904b84f5f9af03555
Michal Torma <miso.torma@gmail.com>
2021-09-17 13:47:51 +0200

    Add stats

--- scripts/species/04_consolidate_image_urls.py
+++ scripts/species/04_consolidate_image_urls.py
@@ -16,3 +16,15 @@ if __name__ == '__main__':
     res = pd.concat(list_of_multimedia_dfs)
     res.to_csv(f'partial{os.sep}04_species_urls.csv')
 # %%
+def stats(df, treshold_num=10):
+    input_num = df.groupby(['gbifKey']).agg(lambda x: len(x))
+    input_num = input_num[['identifier']]
+    input_num.columns = ['count']
+
+    return {
+        'Treshold' : treshold_num,
+        'Number of species with observation' : len(input_num),
+        'Number of species with more than treshold' : {sum(input_num['count']>treshold_num)},}
+
+
+# %%
