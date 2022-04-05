46ac20606f7a590cb93185e18d990cf4ea9ded14
Michal Torma <michal.torma@nhm.uio.no>
2022-04-05 10:31:20 +0200

    Finish api

--- scripts/database/src/new_api/artsdatabanken-nin-api.py
+++ scripts/database/src/new_api/artsdatabanken-nin-api.py
@@ -1,12 +1,16 @@
+# %%
 import requests
 import json
 
 
 class ArtsdatabankenNinApi:
-    base_url = 'https://nin-kode-api.artsdatabanken.no'
+    # base_url = 'https://nin-kode-api.artsdatabanken.no'
     headers = {
         'Accept': 'text/plain'
     }
+
+    def __init__(self, base_url) -> None:
+        self.base_url = base_url
 
     def get_versions(self):
         url = f'{self.base_url}/versjoner'
@@ -15,5 +19,10 @@ class ArtsdatabankenNinApi:
 
     def get_all_codes(self, version):
         url = f'{self.base_url}/{version}/koder/allekoder?tre=false'
+        response = requests.get(url=url, headers=self.headers)
+        return json.loads(response.content)
+
+    def get_specific_code(self, version, code):
+        url = f'{self.base_url}/{version}/koder/hentkode/{code}'
         response = requests.get(url=url, headers=self.headers)
         return json.loads(response.content)
