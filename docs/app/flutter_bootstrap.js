249ae5fb4b9f801f4a02102a23791fce43a8aa4b
Michal Torma <miso.torma.dev@gmail.com>
2026-05-28 02:35:59 +0200

    Deploy web app v3.0.2+47
    Built with base-href /natur-i-norge/app/ for GitHub Pages.

--- docs/app/flutter_bootstrap.js
+++ docs/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1577642418" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3906802990" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
